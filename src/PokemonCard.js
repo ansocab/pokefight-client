4c031332b0e7440a4cf3df7c06a5c1e886ea3c74
Ana Caballero <ana.caballero@gmx.de>
2020-12-09 09:36:09 +0100

    create game route, add router link on cards

--- src/PokemonCard.js
+++ src/PokemonCard.js
@@ -1,54 +1,59 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./App.css";
 
-export default function PokemonCard(props, key) {
+export default function PokemonCard(props) {
+
+  const ConditionalWrapper = ({ condition, wrapper, children }) => condition ? wrapper(children) : children;
+  
   return (
-    
     <div className="Pokemonclass">
-      <div className="card__image-container">
-        <img
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${props.id}.png`}
-          alt="Eevee"
-          className="card__image"
-        />
-      </div>
+      <ConditionalWrapper condition={(props.origin === "list")} wrapper={children => <Link to={`fight/${props.id}`}>{children}</Link>}>
+        <div className="card__image-container">
+          <img
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${props.id}.png`}
+            alt="Eevee"
+            className="card__image"
+          />
+        </div>
 
-      <figcaption className="card__caption">
-        <h1 className="card__name">{props.name.english}</h1>
+        <figcaption className="card__caption">
+          <h1 className="card__name">{props.name.english}</h1>
 
-        <h3 className="card__type">{props.type.join("-")}</h3>
+          <h3 className="card__type">{props.type.join("-")}</h3>
 
-        <table className="card__stats">
-          <tbody>
-            <tr>
-              <th>HP</th>
-              <td>{props.base.HP}</td>
-            </tr>
-            <tr>
-              <th>Attack</th>
-              <td>{props.base.Attack}</td>
-            </tr>
+          <table className="card__stats">
+            <tbody>
+              <tr>
+                <th>HP</th>
+                <td>{props.base.HP}</td>
+              </tr>
+              <tr>
+                <th>Attack</th>
+                <td>{props.base.Attack}</td>
+              </tr>
 
-            <tr>
-              <th>Defense</th>
-              <td>{props.base.Defense}</td>
-            </tr>
+              <tr>
+                <th>Defense</th>
+                <td>{props.base.Defense}</td>
+              </tr>
 
-            <tr>
-              <th>Special Attack</th>
-              <td>{props.base["Sp. Attack"]}</td>
-            </tr>
-            <tr>
-              <th>Special Defense</th>
-              <td>{props.base["Sp. Defense"]}</td>
-            </tr>
-            <tr>
-              <th>Speed</th>
-              <td>{props.base.Speed}</td>
-            </tr>
-          </tbody>
-        </table>
-      </figcaption>
+              <tr>
+                <th>Special Attack</th>
+                <td>{props.base["Sp. Attack"]}</td>
+              </tr>
+              <tr>
+                <th>Special Defense</th>
+                <td>{props.base["Sp. Defense"]}</td>
+              </tr>
+              <tr>
+                <th>Speed</th>
+                <td>{props.base.Speed}</td>
+              </tr>
+            </tbody>
+          </table>
+        </figcaption>
+        </ConditionalWrapper>
     </div>
   );
 }
