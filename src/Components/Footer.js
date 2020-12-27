4d338b2f87656beebb24945e2a5fb86403cf5177
Ana Caballero <71068644+ansocab@users.noreply.github.com>
2020-12-27 14:21:14 +0100

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,7 +6,7 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 export default function Footer() {
   return (
     <div className="footer-wrapper">
-      <p>By Ana Caballero | GitHub Repo:</p>
+      <p>GitHub Repo:</p>
       <span className="p-2"><a href="https://github.com/ansocab/pokefight-client" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub}/></a></span>
     </div>
   );
